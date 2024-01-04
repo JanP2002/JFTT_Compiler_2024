28a51a9e6f445f56de53602ad4e7f008c0a9a8d9
jan <268446@student.pwr.edu.pl>
2024-01-04 21:58:08 +0100

    Implements operations on constant inside procedures
    Restores command translation to Command's child classes

--- NonTerminals/Command.py
+++ NonTerminals/Command.py
@@ -1,6 +1,9 @@
 import Instructions
 from typing import List
+
+from MemoryManager import MemoryManager, MemoryManagerException
 from NonTerminals.ProcCallParam import ProcCallParam
+from Register import REG
 
 
 class Command:
@@ -26,7 +29,9 @@ class CommandWriteNum(Command):
         self.num = num
 
     def translate(self, p):
-        return Instructions.write_num(self.num)
+        asm_code = Instructions.set_register_const(REG.A, self.num)
+        asm_code.append(Instructions.makeInstr('WRITE'))
+        return asm_code
 
 
 class CommandWritePid(Command):
@@ -35,7 +40,38 @@ class CommandWritePid(Command):
         self.pid = pid
 
     def translate(self, p):
-        return Instructions.write_pid(self.pid, self.line_number, self.parent_procedure)
+        memory_manager: MemoryManager = MemoryManager()
+        asm_code = []
+        if self.parent_procedure is not None:
+            variable_id = self.parent_procedure + "##" + self.pid
+            declaration = memory_manager.get_variable(variable_id, self.line_number)
+            if declaration.is_param:
+                if (not declaration.is_initialized) and (not declaration.must_be_initialized):
+                    declaration.set_uninitialized_error(self.pid, self.line_number)
+                address = declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.A.value))
+                asm_code.append(Instructions.makeInstr('WRITE'))
+            else:
+                if not declaration.is_initialized:
+                    raise MemoryManagerException("Blad w linii %i: Zmienna %s nie jest zainicjalizowana" %
+                                                 (self.line_number, declaration.pid))
+                address = declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))
+                asm_code.append(Instructions.makeInstr('WRITE'))
+        else:
+            declaration = memory_manager.get_variable(self.pid, self.line_number)
+            if not declaration.is_initialized:
+                raise MemoryManagerException("Blad w linii %i: Zmienna %s nie jest zainicjalizowana" %
+                                             (self.line_number, declaration.pid))
+            address = declaration.get_memory_id()
+            asm_code.extend(Instructions.set_register_const(REG.B, address))
+            asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))
+            asm_code.append(Instructions.makeInstr('WRITE'))
+
+        return asm_code
 
 
 class CommandReadPid(Command):
@@ -44,7 +80,34 @@ class CommandReadPid(Command):
         self.pid = pid
 
     def translate(self, p):
-        return Instructions.read_pid(self.pid, self.line_number, self.parent_procedure)
+        memory_manager: MemoryManager = MemoryManager()
+        asm_code = []
+        if self.parent_procedure is not None:
+            variable_id = self.parent_procedure + "##" + self.pid
+            declaration = memory_manager.get_variable(variable_id, self.line_number)
+            address = declaration.get_memory_id()
+            if declaration.is_param:
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))  # w A mamy teraz adres zmiennej pid
+                asm_code.append(Instructions.makeInstr('PUT', REG.B.value))  # w B mamy teraz adres zmiennej pid
+                asm_code.append(Instructions.makeInstr('READ'))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                # declaration.must_be_initialized = False
+            else:
+                asm_code.append(Instructions.makeInstr('READ'))
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+            declaration.is_initialized = True
+
+        else:
+            declaration = memory_manager.get_variable(self.pid, self.line_number)
+            address = declaration.get_memory_id()
+            asm_code.append(Instructions.makeInstr('READ'))
+            asm_code.extend(Instructions.set_register_const(REG.B, address))
+            asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+            declaration.is_initialized = True
+
+        return asm_code
 
 
 class CommandPidAssignNumber(Command):
@@ -54,7 +117,33 @@ class CommandPidAssignNumber(Command):
         self.number = number
 
     def translate(self, p):
-        return Instructions.pid_assign_number(self.pid, self.number, self.line_number, self.parent_procedure)
+        memory_manager: MemoryManager = MemoryManager()
+        asm_code = []
+        if self.parent_procedure is not None:
+            variable_id = self.parent_procedure + "##" + self.pid
+            declaration = memory_manager.get_variable(variable_id, self.line_number)
+            if declaration.is_param:
+                address = declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))  # w A mamy teraz adres zmiennej pid
+                asm_code.append(Instructions.makeInstr('PUT', REG.B.value))  # w B mamy teraz adres zmiennej pid
+                asm_code.extend(Instructions.set_register_const(REG.A, self.number))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                declaration.is_initialized = True
+            else:
+                address = declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.A, self.number))
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                declaration.is_initialized = True
+        else:
+            declaration = memory_manager.get_variable(self.pid, self.line_number)
+            address = declaration.get_memory_id()
+            asm_code.extend(Instructions.set_register_const(REG.A, self.number))
+            asm_code.extend(Instructions.set_register_const(REG.B, address))
+            asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+            declaration.is_initialized = True
+        return asm_code
 
 
 class CommandPidAssignPid(Command):
@@ -64,7 +153,78 @@ class CommandPidAssignPid(Command):
         self.right_pid = r_pid
 
     def translate(self, p):
-        return Instructions.pid_assign_pid(self.left_pid, self.right_pid, self.line_number, self.parent_procedure)
+        memory_manager: MemoryManager = MemoryManager()
+        asm_code = []
+        if self.parent_procedure is not None:
+            l_variable_id = self.parent_procedure + "##" + self.left_pid
+            r_variable_id = self.parent_procedure + "##" + self.right_pid
+            l_declaration = memory_manager.get_variable(l_variable_id, self.line_number)
+            r_declaration = memory_manager.get_variable(r_variable_id, self.line_number)
+            if l_declaration.is_local and r_declaration.is_local:
+                if not r_declaration.is_initialized:
+                    raise MemoryManagerException(
+                        "Blad w linii %i: nizainicjowana zmienna %s" % (self.line_number, self.right_pid))
+                l_address = l_declaration.get_memory_id()
+                r_address = r_declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.E, r_address))  # adres zmiennej right_pid w reg e
+                asm_code.append(Instructions.makeInstr('LOAD', REG.E.value))
+                asm_code.extend(Instructions.set_register_const(REG.B, l_address))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                l_declaration.is_initialized = True
+            elif l_declaration.is_param and r_declaration.is_local:
+                if not r_declaration.is_initialized:
+                    raise MemoryManagerException(
+                        "Blad w linii %i: nizainicjowana zmienna %s" % (self.line_number, self.right_pid))
+                l_address = l_declaration.get_memory_id()
+                r_address = r_declaration.get_memory_id()  # adres zmiennej right_pid
+                asm_code.extend(Instructions.set_register_const(REG.B, l_address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))  # w A mamy teraz adres zmiennej left_pid
+                asm_code.append(Instructions.makeInstr('PUT', REG.B.value))  # w B mamy teraz adres zmiennej left_pid
+                asm_code.extend(Instructions.set_register_const(REG.E, r_address))  # adres zmiennej right_pid w reg e
+                asm_code.append(Instructions.makeInstr('LOAD', REG.E.value))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                l_declaration.is_initialized = True
+            elif l_declaration.is_local and r_declaration.is_param:
+                if (not r_declaration.is_initialized) and (not r_declaration.must_be_initialized):
+                    r_declaration.set_uninitialized_error(self.right_pid, self.line_number)
+                l_address = l_declaration.get_memory_id()  # adres zmiennej left_pid
+                r_address = r_declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.E, r_address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.E.value))  # w A mamy teraz adres zmiennej right_pid
+                # asm_code.append(makeInstr('PUT', REG.E.value))  # w E mamy teraz adres zmiennej right_pid
+                asm_code.append(Instructions.makeInstr('LOAD', REG.A.value))  # w A mamy teraz wartosc zmiennej right_pid
+                asm_code.extend(Instructions.set_register_const(REG.B, l_address))
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                l_declaration.is_initialized = True
+            else:  # l_declaration.is_param and r_declaration.is_param
+                if (not r_declaration.is_initialized) and (not r_declaration.must_be_initialized):
+                    r_declaration.set_uninitialized_error(self.right_pid, self.line_number)
+                l_address = l_declaration.get_memory_id()
+                r_address = r_declaration.get_memory_id()
+                asm_code.extend(Instructions.set_register_const(REG.B, l_address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.B.value))  # w A mamy teraz adres zmiennej left_pid
+                asm_code.append(Instructions.makeInstr('PUT', REG.B.value))  # w B mamy teraz adres zmiennej left_pid
+
+                asm_code.extend(Instructions.set_register_const(REG.E, r_address))
+                asm_code.append(Instructions.makeInstr('LOAD', REG.E.value))  # w A mamy teraz adres zmiennej right_pid
+                # asm_code.append(makeInstr('PUT', REG.E.value))  # w E mamy teraz adres zmiennej right_pid
+                asm_code.append(Instructions.makeInstr('LOAD', REG.A.value))  # w A mamy teraz wartosc zmiennej right_pid
+                asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+                l_declaration.is_initialized = True
+        else:
+            l_declaration = memory_manager.get_variable(self.left_pid, self.line_number)
+            r_declaration = memory_manager.get_variable(self.right_pid, self.line_number)
+            if not r_declaration.is_initialized:
+                raise MemoryManagerException("Blad w linii %i: nizainicjowana zmienna %s" % (self.line_number,
+                                                                                             self.right_pid))
+            l_address = l_declaration.get_memory_id()
+            r_address = r_declaration.get_memory_id()
+            asm_code.extend(Instructions.set_register_const(REG.E, r_address))  # adres zmiennej right_pid w reg e
+            asm_code.append(Instructions.makeInstr('LOAD', REG.E.value))
+            asm_code.extend(Instructions.set_register_const(REG.B, l_address))
+            asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+            l_declaration.is_initialized = True
+        return asm_code
 
 
 class CommandPidAssignNumOpNum(Command):
@@ -76,8 +236,60 @@ class CommandPidAssignNumOpNum(Command):
         self.operation = operation
 
     def translate(self, p):
-        return Instructions.pid_assign_num_op_num(self.pid, self.num1, self.num2,
-                                                  self.operation, self.line_number, self.parent_procedure)
+        memory_manager: MemoryManager = MemoryManager()
+        asm_code = []
+
+        # asm_code.extend(set_register_const(REG.A, num1))
+        # asm_code.extend(set_register_const(REG.B, num2))
+        # if operation == '+':
+        #     asm_code.extend(generate_adding(num1, num2))
+        # elif operation == '-':
+        #     asm_code.extend(generate_subtraction(num1, num2))
+        # else:
+        #     raise Exception("Nieprawidlowa operacja")
+
+        if self.parent_procedure is not None:
+            variable_id = self.parent_procedure + '##' + self.pid
+            declaration = memory_manager.get_variable(variable_id, self.line_number)
+            address = declaration.get_memory_id()
+            if declaration.is_param:
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+                asm_code.append(Instructions.LOAD(REG.B))  # w A mamy teraz adres zmiennej pid
+                asm_code.append(Instructions.makeInstr('PUT', REG.B.value))  # w B mamy teraz adres zmiennej pid
+
+            else:
+                asm_code.extend(Instructions.set_register_const(REG.B, address))
+        else:
+            declaration = memory_manager.get_variable(self.pid, self.line_number)
+            address = declaration.get_memory_id()
+            asm_code.extend(Instructions.set_register_const(REG.B, address))
+
+        if self.operation == '+':
+            result = self.num1 + self.num2
+            asm_code.extend(Instructions.set_register_const(REG.A, result))
+        elif self.operation == '-':
+            result = 0
+            if self.num2 < self.num1:
+                result = self.num1 - self.num2
+            asm_code.extend(Instructions.set_register_const(REG.A, result))
+        elif self.operation == "*":
+            result = self.num1 * self.num2
+            asm_code.extend(Instructions.set_register_const(REG.A, result))
+        elif self.operation == '/':
+            result = 0
+            if self.num2 != 0:
+                result = self.num1 // self.num2
+            asm_code.extend(Instructions.set_register_const(REG.A, result))
+        elif self.operation == '%':
+            result = 0
+            if self.num2 != 0:
+                result = self.num1 % self.num2
+            asm_code.extend(Instructions.set_register_const(REG.A, result))
+        else:
+            raise Exception("Nieprawidlowa operacja")
+        asm_code.append(Instructions.makeInstr('STORE', REG.B.value))
+        declaration.is_initialized = True
+        return asm_code
 
 
 class ProcCall(Command):
